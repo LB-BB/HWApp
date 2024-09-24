f4bd44087378db9fc50d15811262e32d6e347193
LB-BB <57300241+LB-BB@users.noreply.github.com>
2024-09-24 14:09:21 -0700

    add useToggle

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import {
+  Button,
   SafeAreaView,
   Text,
   View
 } from 'react-native';
+import useToggle from './customHooks/useToggle';
 
 const App = () => {
+  const [isOn, toggleIsOn] = useToggle(false)
   return (
     <SafeAreaView>
       <View>
-        <Text>Hello World!</Text>
+        <Text>{isOn ? 'ON' : 'OFF'}</Text>
+        <Button title='Toggle' onPress={toggleIsOn} />
       </View>
     </SafeAreaView>
   )
