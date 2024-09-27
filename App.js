e96f0d2d12816fc76f9f9f28488c91d5cb90307c
LB-BB <57300241+LB-BB@users.noreply.github.com>
2024-09-27 16:20:25 -0700

    text inputs

--- App.js
+++ App.js
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
-  Text,
-  View
+  TextInput,
 } from 'react-native';
 
 const App = () => {
+  const [textValue, setTextValue] = useState('');
+  const [emailValue, setEmailValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
   return (
     <SafeAreaView>
-      <View>
-        <Text>Hello World!</Text>
-      </View>
+      <TextInput
+      style={{borderWidth: 1, padding: 10, borderRadius: 4}}
+      value={textValue}
+      onChangeText={(value) => {
+        setTextValue(value);
+      }}
+      autoFocus={true}
+      placeholder="Please enter your name"/>
+      <TextInput
+      style={{borderWidth: 1, padding: 10, borderRadius: 4}}
+      value={emailValue}
+      onChangeText={(value) => {
+        setEmailValue(value);
+      }}
+      autoFocus={true}
+      keyboardType="email-address"
+      placeholder="Please enter your email"
+      returnKeyType="go"/>
+      <TextInput
+      style={{borderWidth: 1, padding: 10, borderRadius: 4}}
+      value={passwordValue}
+      onChangeText={(value) => {
+        setPasswordValue(value);
+      }}
+      autoFocus={true}
+      secureTextEntry={true}
+      keyboardType={'phone-pad'}
+      placeholder={'Please enter your password'}/>
     </SafeAreaView>
-  )
+  );
 };
 
 export default App;
