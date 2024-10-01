cd4124e4f84609d96a7ee830efd646affa0d9901
LB-BB <57300241+LB-BB@users.noreply.github.com>
2024-09-30 20:31:27 -0700

    inputs, toggle, touchableOpacity, icons

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
 import React, {useState} from 'react';
 import {
+  View,
+  Text,
+  TouchableOpacity,
   SafeAreaView,
   TextInput,
+  Switch,
 } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 const App = () => {
-  const [textValue, setTextValue] = useState('');
   const [emailValue, setEmailValue] = useState('');
   const [passwordValue, setPasswordValue] = useState('');
+  const [shouldKeepLoggedIn, setShouldKeepLoggedIn] = useState(true);
   return (
     <SafeAreaView>
-      <TextInput
-      style={{borderWidth: 1, padding: 10, borderRadius: 4}}
-      value={textValue}
-      onChangeText={(value) => {
-        setTextValue(value);
-      }}
-      autoFocus={true}
-      placeholder="Please enter your name"/>
       <TextInput
       style={{borderWidth: 1, padding: 10, borderRadius: 4}}
       value={emailValue}
@@ -36,8 +34,31 @@ const App = () => {
       }}
       autoFocus={true}
       secureTextEntry={true}
-      keyboardType={'phone-pad'}
       placeholder={'Please enter your password'}/>
+      <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
+        <Switch
+        value={shouldKeepLoggedIn}
+        onValueChange={(value) => setShouldKeepLoggedIn(value)}
+        />
+        <Text style={{marginLeft: 10}}>Keep me logged in</Text>
+       </View>
+      <TouchableOpacity
+      style={[{backgroundColor: 'black'}, (emailValue.length === 0 && {opacity: 0.5})]}
+      disabled={emailValue.length === 0 || passwordValue.length < 8}
+      onPress={() => {
+        console.log(emailValue, passwordValue, shouldKeepLoggedIn);
+      }}>
+        <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+          <Text style={{
+            color: 'white',
+            textAlign: 'center',
+            padding: 10,
+            }}>
+            Submit
+          </Text>
+          <FontAwesomeIcon icon={faCheck} style={{color: 'white'}} />
+        </View>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
